8ac0085e5e57bb2b44a1b40161734eee54772693
André Padeti <andrepadeti@hotmail.com>
2021-10-25 17:16:26 -0300

    chore: Add reset to styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from 'styles/globalStyles'
+
 function App() {
-  return <h1>Space app</h1>
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Space app</h1>
+    </>
+  )
 }
 
 export default App
