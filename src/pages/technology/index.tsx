b12ed5a4ec9c90edf3dd0c4f6e925f661e2c09a9
André Padeti <andrepadeti@hotmail.com>
2021-10-26 15:33:27 -0300

    chore: Organise file structure part 2

---
+++ src/pages/technology/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Title from 'components/common/title'
+import Stuff from 'components/technology/stuff'
+
+const Destination = () => {
+  return (
+    <div>
+      <Title title="technology" />
+      <Stuff />
+    </div>
+  )
+}
+
+export default Destination
