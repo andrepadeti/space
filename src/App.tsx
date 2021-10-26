43ac706f74f8ab7b2ceb564c3877f4bc0739a1b3
André Padeti <andrepadeti@hotmail.com>
2021-10-26 15:13:30 -0300

    chore: Organise file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { GlobalStyles } from 'styles/globalStyles'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navigation from 'components/common/header/navigation'
+import Home from 'pages/home'
+import Destination from 'pages/destination'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>Space app</h1>
+      <Router>
+      <Navigation />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/destination">
+          <Destination />
+        </Route>
+      </Router>
     </>
   )
 }
