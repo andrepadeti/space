4b9c15a71e73028f51b622b650b2158211182fa0
André Padeti <andrepadeti@hotmail.com>
2021-10-26 22:57:13 -0300

    chore: Create media query hook

---
+++ src/utils/media-query.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+interface Dictionary {
+  [index: string]: number
+}
+
+interface MediaQueries {
+  [index: string]: string
+}
+
+/**
+ * Media query hook
+ * @example
+ * import useMediaQuery, {device} from 'utils/media-query'
+ * const isMobile = useMediaQuery(device.mobile)
+ * const isTablet = useMediaQuery(device.tablet)
+ * const isDesktop = useMediaQuery(device.desktop)
+ * @param query
+ * @returns boolean
+ */
+export default function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = React.useState(false)
+
+  React.useEffect(() => {
+    const media = window.matchMedia(query)
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+    const listener = () => {
+      setMatches(media.matches)
+    }
+    media.addEventListener('change', listener)
+    return () => media.removeEventListener('change', listener)
+  }, [matches, query])
+
+  return matches
+}
+
+const size: Dictionary = {
+  sm: 375,
+  md: 768,
+  lg: 1200,
+}
+
+export const device: MediaQueries = {
+  mobile: `(max-width: ${size.sm}px)`,
+  tablet: `(min-width: ${size.sm + 1}px) and (max-width: ${size.md}px)`,
+  desktop: `(min-width: ${size.md + 1}px)`,
+}
