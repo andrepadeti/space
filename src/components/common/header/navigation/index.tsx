080b108e7f0bde83d3d124c2dcc144f5a80472f3
André Padeti <andrepadeti@hotmail.com>
2021-10-31 00:42:18 -0300

    feat: First draft <Navigation /> component

--- src/components/common/header/navigation/index.tsx
+++ src/components/common/header/navigation/index.tsx
@@ -1,26 +1,93 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
+import useMediaQuery from 'utils/media-query-all'
 
-import { Container } from './styles'
+import * as S from './styles'
+import logo from 'assets/shared/logo.svg'
+import close from 'assets/shared/icon-close.svg'
+import hamburger from 'assets/shared/icon-hamburger.svg'
 
 const Navigation = () => {
+  const [showMenu, setShowMenu] = React.useState(true)
+  const mq = useMediaQuery()
+  const { isDesktop, isTablet, isMobile } = mq
+
+  const renderNavigation = () => {
+    return (
+      <>
+        <S.MenuItems>
+          <S.MenuLink to="/">
+            <span>00</span>Home
+          </S.MenuLink>
+        </S.MenuItems>
+        <S.MenuItems>
+          <S.MenuLink to="/destination">
+            <span>01</span>Destination
+          </S.MenuLink>
+        </S.MenuItems>
+        <S.MenuItems>
+          <S.MenuLink to="/crew">
+            <span>02</span>Crew
+          </S.MenuLink>
+        </S.MenuItems>
+        <S.MenuItems>
+          <S.MenuLink to="/technology">
+            <span>03</span>Technology
+          </S.MenuLink>
+        </S.MenuItems>
+      </>
+    )
+  }
+
+  const renderLargeScreenNav = () => {
+    return (
+      <S.Container mq={mq}>
+        <S.IconWrapper left={true}>
+          <img src={logo} alt="Logo" />
+        </S.IconWrapper>
+
+        {isDesktop ? <S.Line /> : null}
+
+        <nav>
+          <S.MenuWrapper mq={mq}>
+            {renderNavigation()}
+          </S.MenuWrapper>
+        </nav>
+      </S.Container>
+    )
+  }
+
+  const renderSmallScreenNav = () => {
+    return (
+      <S.MobileContainer>
+        <S.IconWrapper left={true}>
+          <img src={logo} alt="Logo" />
+        </S.IconWrapper>
+
+        <S.IconWrapper right={true}>
+          <img src={hamburger} alt="Logo" onClick={() => setShowMenu(true)} />
+        </S.IconWrapper>
+
+        <S.Navigation
+          mq={mq}
+          show={showMenu}
+          onClick={() => setShowMenu(false)}
+        >
+          <S.MenuWrapper mq={mq}>
+            <S.IconWrapper close={true}>
+              <img src={close} alt="Logo" onClick={() => setShowMenu(true)} />
+            </S.IconWrapper>
+            {renderNavigation()}
+          </S.MenuWrapper>
+        </S.Navigation>
+      </S.MobileContainer>
+    )
+  }
+
   return (
-    <nav>
-      <Container>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/destination">Destination</Link>
-        </li>
-        <li>
-          <Link to="/crew">Crew</Link>
-        </li>
-        <li>
-          <Link to="/technology">Technology</Link>
-        </li>
-      </Container>
-    </nav>
+    <>
+      {isDesktop || isTablet ? renderLargeScreenNav() : null}
+      {isMobile ? renderSmallScreenNav() : null}
+    </>
   )
 }
 
