021f429fd8875f692e78749d50c6bd0a6c93e1a0
André Padeti <andrepadeti@hotmail.com>
2021-11-04 00:03:53 -0300

    feat: Destination page - mobile only (for now)

--- src/pages/destination/index.tsx
+++ src/pages/destination/index.tsx
@@ -1,13 +1,45 @@
 import * as React from 'react'
-import Title from 'components/common/title'
-import Stuff from 'components/destination/stuff'
+import data from 'utils/data.json'
+
+import * as S from './index.style'
+import Image from 'components/common/image'
+
+import pic from 'assets/destination/image-europa.webp'
 
 const Destination = () => {
+  const { destinations } = data
+  const [tabIndex, setTabIndex] = React.useState(0)
+
   return (
-    <div>
-      <Title number='01' title="destination" />
-      <Stuff />
-    </div>
+    <S.Grid>
+      <S.PageTitle number="01" title="pick your destination" />
+      <S.ImageContainer>
+        <Image index={tabIndex} component="destination" />
+      </S.ImageContainer>
+
+      <S.SwitcherContainer>
+        {destinations.map((destination, index) => (
+          <S.Switcher
+            key={'dest' + index}
+            active={index === tabIndex}
+            onClick={() => setTabIndex(index)}
+          >
+            {destination.name}
+          </S.Switcher>
+        ))}
+      </S.SwitcherContainer>
+      <S.ContentContainer>
+        <S.Title>{destinations[tabIndex].name}</S.Title>
+        <S.Description>{destinations[tabIndex].description}</S.Description>
+        <S.Hr></S.Hr>
+        <S.StatsContainer>
+          <S.StatsTitle>Avg. Distance</S.StatsTitle>
+          <S.StatsFigure>{destinations[tabIndex].distance}</S.StatsFigure>
+          <S.StatsTitle>Est. Travel Time</S.StatsTitle>
+          <S.StatsFigure>{destinations[tabIndex].travel}</S.StatsFigure>
+        </S.StatsContainer>
+      </S.ContentContainer>
+    </S.Grid>
   )
 }
 
