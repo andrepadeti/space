3d9d1504d0216e2cab2954ec19b1d14fefe2e27d
Serge <s.a.kostrikin@gmail.com>
2021-11-07 08:12:31 -0500

    home-component desktop

--- src/components/home/HomeStyles.ts
+++ src/components/home/HomeStyles.ts
@@ -2,12 +2,28 @@ import styled from 'styled-components'
 
 export const HomeSectionWrapper = styled.div`
   @media (min-width: 576px) {
-    width: 444px;
+    min-width: 444px;
     margin-left: auto;
     margin-right: auto;
     margin-top: 50px;
   }
+
+  @media (min-width: 850px) {
+    max-width: 1440px;
+    /* width: auto; */
+    margin-top: 190px;
+    /* grid-template-columns: 1fr 1fr;
+    display: grid;
+    grid-template-areas:
+      'subtitle .'
+      'title exploreBlock'
+      'description exploreBlock'; */
+    display: flex;
+    justify-content: space-between;
+  }
 `
+
+export const MainTextWrapper = styled.div``
 
 export const SubTitle = styled.p`
   font-family: var(--ff-sans-cond);
@@ -17,6 +33,11 @@ export const SubTitle = styled.p`
   font-weight: normal;
   letter-spacing: 3.38px;
   text-transform: uppercase;
+
+  @media (min-width: 850px) {
+    /* grid-area: subtitle; */
+    text-align: left;
+  }
 `
 export const Title = styled.h1`
   font-family: var(--ff-serif);
@@ -25,7 +46,13 @@ export const Title = styled.h1`
   color: hsla(var(--c-white));
   font-weight: normal;
   text-transform: uppercase;
+
+  @media (min-width: 850px) {
+    /* grid-area: title; */
+    text-align: left;
+  }
 `
+export const ExploreBlockWrapper = styled.div``
 
 export const ExploreBlock = styled.div`
   width: 150px;
@@ -49,5 +76,10 @@ export const ExploreBlock = styled.div`
     width: 242px;
     height: 242px;
     font-size: var(--fs-4);
+  }
+
+  @media (min-width: 850px) {
+    margin-top: 120px;
+    /* grid-area: exploreBlock; */
   }
 `
